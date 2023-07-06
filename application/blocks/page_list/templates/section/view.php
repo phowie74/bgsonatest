ca027aebdfa4da2d9b8808b24b71a8f1836aeb96
phowie74 <phowie@bluegreendesign.co.uk>
2023-07-06 12:14:41 +0100

    set up product template

--- application/blocks/page_list/templates/section/view.php
+++ application/blocks/page_list/templates/section/view.php
@@ -5,22 +5,23 @@ $th = Core::make('helper/text');
 if (is_object($c) && $c->isEditMode() && $controller->isBlockEmpty()) {
 ?>
 <div class="ccm-edit-mode-disabled-item"><?php echo t('Empty Page List Block.') ?></div>
-<?php 
-} else {
-    echo '<div class="layout-grid">';
-    foreach ($pages as $page) {
-        $title = $page->getCollectionName();
-        $target = '_self';
-        if ($page->getCollectionPointerExternalLink() != '') {
-            $url = $page->getCollectionPointerExternalLink();
-            if ($page->openCollectionPointerExternalLinkInNewWindow()) {
-                $target = '_blank';
-            }
-        } else {
-            $url = $page->getCollectionLink();
-            $target = $page->getAttribute('nav_target');
-        }
+<?php } else { ?>
+<div class="layout-grid">
+<?php
+foreach ($pages as $page) {
+    $title = $page->getCollectionName();
+    $target = '_self';
+    if ($page->getCollectionPointerExternalLink() != '') {
+        $url = $page->getCollectionPointerExternalLink();
+        if ($page->openCollectionPointerExternalLinkInNewWindow()) { $target = '_blank'; }
+    } else {
+        $url = $page->getCollectionLink();
+        $target = $page->getAttribute('nav_target');
+    }
 ?>
+
+
+
 <a href="<?php echo h($url) ?>" target="<?php echo h($target) ?>">
 <?php echo h($title) ?>
 </a>
